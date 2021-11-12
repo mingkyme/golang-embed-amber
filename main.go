c9b5f878110dbe1b3416391695fc1a5f11aa9311
MINGKYME <admin@mingky.me>
2021-11-12 21:49:34 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"embed"
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/amber"
+)
+
+//go:embed views/*
+var views embed.FS
+
+func main(){
+	
+	engine := amber.NewFileSystem(http.FS(views), ".amber")
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+
+    app.Get("/", func(c *fiber.Ctx) error {
+        return c.Render("views/index",fiber.Map{})
+    })
+
+    app.Listen(":8080")
+
+}
